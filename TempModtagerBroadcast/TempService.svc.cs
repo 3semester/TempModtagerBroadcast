436a50090d8ddc770de7253fd2499d00e3bacd96
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-11-27 11:35:07 +0100

    Ændret navne samt tilføjet connectionString

---
+++ TempModtagerBroadcast/TempService.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TempModtagerBroadcast
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TempService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select TempService.svc or TempService.svc.cs at the Solution Explorer and start debugging.
+    public class TempService : ITempService
+    {
+        private const string ConnString =
+            "Server=tcp:skole.database.windows.net,1433;Initial Catalog=General_DB;Persist Security Info=False;User ID=lisa;Password=Secret1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        public IList<Temp> GetAllTemp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PostTempToDB(string temp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
